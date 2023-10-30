43b6eb61098ac086d3de219e718ed94f1407e81a
sydjefel <sydjefel@ada.local.isima.fr>
2023-10-30 16:46:47 +0100

    deviser methode render html en sous methode

--- src/main/java/theatricalplays/HtmlStatementRenderer.java
+++ src/main/java/theatricalplays/HtmlStatementRenderer.java
@@ -9,19 +9,27 @@ import java.util.Locale;
  * d'un relevé au format HTML à partir d'une facture et d'un ensemble de pièces.
  */
 public class HtmlStatementRenderer implements StatementRenderer {
+
+    private NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+    private StringBuilder result = new StringBuilder();
+    private StatementCalculator calculator = new StatementCalculator();
     
     @Override
     public String render(Invoice invoice, Map<String, Play> plays) {
-        StatementCalculator calculator = new StatementCalculator();
         calculator.calculate(invoice, plays);
 
-        // Formatteur pour afficher les montants dans le format monétaire approprié.
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-        StringBuilder result = new StringBuilder();
+        beginHtml();
+        addHeader(invoice.customer);
+        beginTable();
+        addPerformancesToTable(invoice, plays);
+        endTable();
+        addFooter();
+        endHtml();
 
-        // Récupère les détails du client depuis la facture.
-        Customer customer = invoice.customer;
-
+        return result.toString();
+    }
+    
+    private void beginHtml() {
         // Commence la construction du document HTML.
         result.append("<html><head><title>Statement</title><style>\n" +
                 "table, th, td {\n" +
@@ -31,17 +39,23 @@ public class HtmlStatementRenderer implements StatementRenderer {
                 "padding: 5px;\n" +
                 "}\n" +
                 "</style>\n" + "</head><body> \n");
+    }
 
+    private void addHeader(Customer customer) {
         // Ajoute le nom du client et son ID.
         result.append(String.format("<h1>Statement for %s</h1>", customer.getName()));
-        result.append(String.format("<p>Customer ID: %s</p>", customer.getClientNumber())); // Affiche l'ID du client.
-
+        result.append(String.format("<p>Customer ID: %s</p>", customer.getClientNumber()));
+        
         // Affiche les points de fidélité du client.
         result.append(String.format("<p>Remaining loyalty points: %s</p>", customer.getLoyaltyPoints()));
+    }
 
+    private void beginTable() {
         // Débute la table pour lister les performances.
         result.append("<table><thead><tr><th>Play</th><th>Seats</th><th>Cost</th></tr></thead><tbody>");
+    }
 
+    private void addPerformancesToTable(Invoice invoice, Map<String, Play> plays) {
         // Parcourt et affiche chaque performance dans la table.
         for (Performance perf : invoice.performances) {
             Play play = plays.get(perf.playID);
@@ -51,17 +65,21 @@ public class HtmlStatementRenderer implements StatementRenderer {
                         frmt.format(calculator.calculateAmountForPerformance(play, perf))
             ));
         }
+    }
 
+    private void endTable() {
         // Termine la table.
         result.append("</tbody></table>");
+    }
 
+    private void addFooter() {
         // Affiche le montant total dû et les crédits gagnés.
         result.append(String.format("<p>Amount owed is %s</p>", frmt.format(calculator.totalAmount)));
         result.append(String.format("<p>You earned %s credits</p>", calculator.volumeCredits));
+    }
 
+    private void endHtml() {
         // Termine le document HTML.
         result.append("</body></html>");
-
-        return result.toString();
     }
 }
